3bed36952d4c3c3fdb8ea2bbd1b95f87908ef6a2
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-11-01 16:43:50 -0600

    Add barnyard data and load script [skip ci]

---
+++ data-raw/barnyard.R
@@ -0,0 +1,30 @@
+# process mRNA and repair product data through Seurat pipeline
+
+library(Seurat)
+
+mrna_tab <- read.csv("data-raw/mrna.csv.gz", sep = ',', header = TRUE, row.names = 1)
+haircut_tab <- read.csv("data-raw/haircut.csv.gz", sep = ',', header = TRUE, row.names = 1)
+
+sce_mrna <- CreateSeuratObject(raw.data = mrna_tab) %>%
+  NormalizeData() %>%
+  FindVariableGenes(do.plot = FALSE, y.cutoff = 0.5) %>%
+  ScaleData(display.progress = FALSE) %>%
+  RunPCA(pcs.print = 0) %>%
+  FindClusters(dims.use = 1:6, print.output = FALSE) %>%
+  RunTSNE(dims.use = 1:6)
+
+# TSNEPlot(sce, do.label = TRUE, pt.size = 0.5)
+
+sce_fxn <- SetAssayData(
+  sce_mrna, assay.type = 'CITE',
+  slot = "raw.data",
+  new.data = haircut_tab
+) %>%
+  NormalizeData(assay.type = "CITE", normalization.method = "genesCLR") %>%
+  ScaleData(assay.type = "CITE", display.progress = FALSE)
+
+# FeaturePlot(
+#   sce_fxn,
+#   features.plot = c("Uracil_45", "riboG_44","ENSG00000076248", "ENSG00000172922"),
+#   cols.use = c("lightgrey", "blue")
+# )
