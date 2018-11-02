b6b310d6acf0ddd76de123022b19ca2aebbff28b
Kent Riemondy <kent.riemondy@gmail.com>
2018-11-02 13:39:02 -0600

    add hairpin plotting utility and add rowData to haircut object

--- R/utils.R
+++ R/utils.R
@@ -10,6 +10,8 @@
 #' @importFrom Matrix readMM
 #' @importFrom stringr str_c str_remove
 #' @importFrom readr read_tsv
+#' @importFrom dplyr left_join
+#' @importFrom tibble rownames_to_column column_to_rownames
 #'
 #' @export
 read_10x_matrix <- function(path,
@@ -62,6 +64,10 @@ read_10x_matrix <- function(path,
 #' @param id_delim delimiter to split cell name (default = ".")
 #' @param id_fields index(es) of fields to extract from name for determining sample id from name
 #' (defaults to c(2))
+#' @param adduct_positions optional data.frame with positions of first and second adducts in each hairpin,
+#'  must be a three column data.frame with the first column named hairpin containing with entries that match the adducts
+#'  from haircut_data (i.e. Uracil from entry Uracil_1), a second column named adduct_position1 with the first adduct position,
+#'  and a third column named adduct_position2 with the second adduct position
 #'
 #' @importFrom stringr fixed str_split
 #' @import SingleCellExperiment
@@ -71,7 +77,8 @@ create_haircut <- function(rna_data,
                            haircut_data,
                            id_from_name = TRUE,
                            id_delim = ".",
-                           id_fields = 2){
+                           id_fields = 2,
+                           adduct_positions = NULL){
 
   # keep umis as sparseMatrix
   rna_mat <- as(as.matrix(rna_data), "sparseMatrix")
@@ -82,16 +89,49 @@ create_haircut <- function(rna_data,
   sce_mrna <- SingleCellExperiment(assays = list(
     counts = rna_mat))
 
+  rna_cells <- colnames(rna_mat)
+  hcut_cells <- colnames(hcut_mat)
+
+  # get info for adduct id and position
+  # for now assume rownames of input hcut_matrix have this structure
+  # ADDUCTID_POS
+  hairpin_fields <- stringr::str_split(rownames(hcut_mat),
+                                       "_",
+                                       simplify = T)
+
+  if(ncol(hairpin_fields) != 2){
+    stop("expecting hairpins to be named as ADDUCTNAME_POS, e.g. Uracil_1")
+  }
+
+  hairpin_id <- hairpin_fields[, 1]
+  hairpin_pos <- hairpin_fields[, 2]
+
+  adduct_data <- data.frame(
+    row.names = rownames(hcut_mat),
+    hairpin = hairpin_id,
+    position = hairpin_pos,
+    stringsAsFactors = FALSE
+  )
+
+  if (!is.null(adduct_positions)){
+    adduct_data <- tibble::rownames_to_column(adduct_data, "hairpin_pos")
+    adduct_data <- dplyr::left_join(adduct_data, adduct_positions, by = c("hairpin"))
+    adduct_data <- tibble::column_to_rownames(adduct_data, "hairpin_pos")
+
+    if(nrow(adduct_data) != nrow(hcut_mat)){
+      stop("unable to add adduct_data to rowData for hairpin object")
+    }
+
+  }
+
   sce_hcut <- SingleCellExperiment(assays = list(
-    counts = hcut_mat))
+    counts = hcut_mat),
+    rowData = adduct_data)
 
   expr_list <- ExperimentList(list(rna = sce_mrna,
                                    hcut = sce_hcut))
 
   ## make map between cell ids and experimental assay
-  rna_cells <- colnames(rna_mat)
-  hcut_cells <- colnames(hcut_mat)
-
   nshared_cells <- intersect(rna_cells, hcut_cells)
 
   if(length(nshared_cells) == 0){
@@ -143,5 +183,10 @@ create_haircut <- function(rna_data,
   hce <- MultiAssayExperiment::MultiAssayExperiment(experiments = expr_list,
                                                     colData = colDat,
                                                     sampleMap = sampMap)
+
+  ## set colData for each assay
+  colData(hce[["rna"]]) <- colData(hce)[rna_cells, , drop = F]
+  colData(hce[["hcut"]]) <- colData(hce)[hcut_cells, , drop = F]
+
   hce
 }
