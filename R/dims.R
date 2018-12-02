79ecf770e2d529169a3e8aef74de71cec096e1e3
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-12-01 18:59:36 -0700

    Rename dims file

--- R/dims.R
+++ R/dims.R
@@ -1,9 +1,10 @@
-#' Calculate PCs use irlba
+#' Calculate principal components using irlba
 #'
 #' @param fce fce object
-#' @param expt one of either sce (rna data, the default) or fsce (functional data)
+#' @param expt one of either sce (rna data, the default) or fsce (functional
+#'   data)
 #' @param assay select which assay data to use for PCA, defaults to logcounts
-#' (i.e. log normalized data)
+#'   (i.e. log normalized data)
 #' @param genes vector of genes to include in PCA, defaults to all genes
 #' @param n_pcs number of principle components to return
 #' @param scale perform PCA on input on scaled data, defaults to TRUE
@@ -65,22 +66,27 @@ calc_pca <- function(fce,
 
 #' Generate 2D cell embeddings using UMAP
 #'
-#' @description See (https://umap-learn.readthedocs.io/en/latest/index.html) for
-#' a detailed description of parameters.
+#' See <https://umap-learn.readthedocs.io> for a detailed description of
+#' parameters.
 #'
 #' @param fce fce object
-#' @param expt Data to use for UMAP, one of either sce (rna data, the default) or fsce (functional data)
-#' @param dr select which dimenality reduction data to use for UMAP, defaults to PCA
-#' @param n_dims number of dimensions to pass to UMAP, defaults to all present in dr matrix
-#' @param n_neighbors number of nearest neighbors to use for learning the manifold. Low values will
-#' preserve local structure, at the expense missing higher order organization. Higher values will
-#' capture more global structure but miss fine grained detail. Defaults to 30.
-#' @param min_dist Numeric between 0 and 0.99. min_dist controls how tightly points can be packed
-#'  together in 2D space. Lower values will generate more clumpy projections, but more accurately
-#'  preserve local structure.
+#' @param expt Data to use for UMAP, one of either sce (rna data, the default)
+#'   or fsce (functional data)
+#' @param dr select which dimenality reduction data to use for UMAP, defaults to
+#'   PCA
+#' @param n_dims number of dimensions to pass to UMAP, defaults to all present
+#'   in dr matrix
+#' @param n_neighbors number of nearest neighbors to use for learning the
+#'   manifold. Low values will preserve local structure, at the expense missing
+#'   higher order organization. Higher values will capture more global structure
+#'   but miss fine grained detail. Defaults to 30.
+#' @param min_dist Numeric between 0 and 0.99. min_dist controls how tightly
+#'   points can be packed together in 2D space. Lower values will generate more
+#'   clumpy projections, but more accurately preserve local structure.
 #' @param metric distance metric for UMAP, defaults to pearson.
-#' @param seed seed to generate reproducible UMAP projection. Defaults to no seed.
-#' @param ... additional arguments to pass to [`umap::umap()`]
+#' @param seed seed to generate reproducible UMAP projection. Defaults to no
+#'   seed.
+#' @param ... additional arguments for [`umap::umap()`]
 #'
 #' @return fce object with UMAP values added to reducedDims
 #' @export
@@ -112,7 +118,8 @@ calc_umap <- function(fce,
     dr_mat <- dr_mat[, 1:n_dims]
   }
 
-  umap_res <- umap::umap(dr_mat,
+  umap_res <- umap::umap(
+    dr_mat,
     method = "naive",
     metric = metric,
     random_state = seed,
@@ -130,24 +137,24 @@ calc_umap <- function(fce,
 
 #' Generate 2D cell embeddings using tSNE
 #'
-#' @description See (https://github.com/jkrijthe/Rtsne) for
-#' a detailed description of parameters.
+#' See [`Rtsne::Rtsne()`] for a detailed description of parameters.
 #'
 #' @param fce fce object
-#' @param expt Data to use for tSNE, one of either sce (rna data, the default) or fsce (functional data)
-#' @param dr select which dimenality reduction data to use for tSNE, defaults to PCA
-#' @param n_dims number of dimensions to pass to tSNE, defaults to all present in dr matrix
-#' @param perplexity Defaults to 30.
-#' @param theta  Defaults to 0.5
-#' @param seed seed to generate reproducible tSNE projection. Defaults to no seed.
-#' @param ...  additional arguments to pass to [`Rtsne::Rtsne()`]
+#' @param expt Data to use for tSNE, sce (rna data, the default)
+#'   or fsce (functional data)
+#' @param method dimensionality reduction method for tSNE
+#' @param n_dims number of dimensions for tSNE
+#' @param perplexity tSNE perplexity value.
+#' @param theta  tSNE theta value.
+#' @param seed seed for generate reproducible tSNE projection.
+#' @param ...  additional arguments for [`Rtsne::Rtsne()`]
 #'
 #' @return fce object with tSNE values added to reducedDims
 #'
 #' @export
 calc_tsne <- function(fce,
                       expt = "sce",
-                      dr = "PCA",
+                      method = "PCA",
                       n_dims = NULL,
                       perplexity = 30,
                       theta = 0.5,
@@ -156,23 +163,24 @@ calc_tsne <- function(fce,
 
   ## check inputs
   if (!expt %in% names(assays(fce))) {
-    stop("expt not found in fce object")
+    stop("expt not found in fce object", call. = FALSE)
   }
 
   if (!dr %in% names(reducedDims(fce[[expt]]))) {
-    stop("dr method not found in fce object")
+    stop("method not found in fce object", call. = FALSE)
   }
 
   dr_mat <- reducedDim(fce[[expt]], dr)
 
   if (!is.null(n_dims)) {
     if (n_dims > ncol(dr_mat)) {
-      stop("n_dims larger than dimensality reduction matrix")
+      stop("n_dims larger than dimensality reduction matrix", call. = FALSE)
     }
     dr_mat <- dr_mat[, 1:n_dims]
   }
 
-  tsne_res <- Rtsne::Rtsne(dr_mat,
+  tsne_res <- Rtsne::Rtsne(
+    dr_mat,
     perplexity = perplexity,
     theta = theta,
     check_duplicates = FALSE,
