9a5904aa66846db52e847d39fc2e0c7ceaef5f54
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-12-29 10:18:45 -0700

    Pre-calculate features for sample data

--- data-raw/fsce_small.R
+++ data-raw/fsce_small.R
@@ -8,4 +8,12 @@ fsce_small <- create_fsce(
   )
 )
 
+# pre-calculate PCA, UMAP, t-SNE, and k-means clusters
+var_genes <- calc_var_features(fsce_small, "rnaseq", n = 5000)
+
+fsce_small <- calc_pca(fsce_small, n_pcs = 20, genes = var_genes)
+fsce_small <- calc_umap(fsce_small, n_dims = 6)
+fsce_small <- calc_tsne(fsce_small, n_dims = 6)
+fsce_small <- calc_kmeans(fsce, k = 6)
+
 usethis::use_data(fsce_small, compress = "xz")
