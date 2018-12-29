a39f61567cf7169db93a67f736ff25fca68d9a60
Jay Hesselberth <jay.hesselberth@gmail.com>
2018-12-29 10:30:41 -0700

    Update fsce_small documentation

--- data-raw/fsce_small.R
+++ data-raw/fsce_small.R
@@ -3,8 +3,8 @@ library(usethis)
 
 fsce_small <- create_fsce(
   list(
-    rnaseq = create_sce_rnaseq(scrunchy_data("mrna/")),
-    haircut = create_sce_haircut(scrunchy_data("haircut/"))
+    rnaseq = create_sce_rnaseq(scrunchy_data("mrna")),
+    haircut = create_sce_haircut(scrunchy_data("haircut"))
   )
 )
 
@@ -14,6 +14,6 @@ var_genes <- calc_var_features(fsce_small, "rnaseq", n = 5000)
 fsce_small <- calc_pca(fsce_small, n_pcs = 20, genes = var_genes)
 fsce_small <- calc_umap(fsce_small, n_dims = 6)
 fsce_small <- calc_tsne(fsce_small, n_dims = 6)
-fsce_small <- calc_kmeans(fsce, k = 6)
+fsce_small <- calc_kmeans(fsce_small, k = 6)
 
 usethis::use_data(fsce_small, compress = "xz", overwrite = TRUE)
