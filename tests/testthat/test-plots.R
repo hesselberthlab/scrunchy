94d3e2cb9389a80c0a0f9edd9951e05d9868a00f
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-01-08 13:22:47 -0700

    Fix again

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -2,11 +2,12 @@ context("test-plots")
 
 test_that("plot_heatmap can accept sparseMatrix input", {
   i <- c(1,3:8); j <- c(2,9,6:10); x <- 7 * (1:7)
-  m <- sparseMatrix(i, j, x = x)
+  m <- Matrix::sparseMatrix(i, j, x = x)
 
   p <- plot_heatmap(m)
   expect_is(p, "Heatmap")
 })
+
 test_that("incorrect labels throw an error", {
   expect_error(
     plot_dims(fsce_tidy, UMAP1, UMAP2, k_cluster, labels = LETTERS[1:10]),
