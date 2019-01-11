03ef2f50b5ab374b95f38aff95c8e1211c083604
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-01-11 09:14:14 -0700

    Add input test checks

--- tests/testthat/test-calcs.R
+++ tests/testthat/test-calcs.R
@@ -1,5 +1,63 @@
 context("test-calcs")
 
+test_that("inputs are checked", {
+
+  fsce_empty <- FunctionalSingleCellExperiment(
+    experiments = list(sce_empty = SingleCellExperiment())
+  )
+
+  ## calc_var_features
+  expect_error(
+    calc_var_features(fsce_empty),
+    "not found in fsce"
+  )
+  expect_error(
+    calc_var_features(fsce_empty, expt = "sce_empty"),
+    "not found for expt"
+  )
+
+  ## calc_kmeans
+  expect_error(
+    calc_kmeans(fsce_empty),
+    "not found in fsce"
+  )
+  expect_error(
+    calc_kmeans(fsce_empty, expt = "sce_empty"),
+    "method `PCA` not found in fsce"
+  )
+
+  ## calc_kmeans
+  expect_error(
+    calc_pca(fsce_empty),
+    "not found in fsce"
+  )
+  expect_error(
+    calc_pca(fsce_empty, expt = "sce_empty"),
+    "`logcounts` not found in fsce"
+  )
+
+  ## calc_umap
+  expect_error(
+    calc_umap(fsce_empty),
+    "not found in fsce"
+  )
+  expect_error(
+    calc_umap(fsce_empty, expt = "sce_empty"),
+    "method `PCA` not found in fsce"
+  )
+
+  ## calc_tsne
+  expect_error(
+    calc_tsne(fsce_empty),
+    "not found in fsce"
+  )
+  expect_error(
+    calc_tsne(fsce_empty, expt = "sce_empty"),
+    "method `PCA` not found in fsce"
+  )
+
+})
+
 test_that("calc functions are reproducible with a seed", {
   var_genes <- calc_var_features(fsce_small, "rnaseq", n = 50)
 
