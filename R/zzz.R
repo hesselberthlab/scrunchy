056528cd7eb620c864c319d6ec30a5a76d18d6f9
Jay Hesselberth <jay.hesselberth@gmail.com>
2019-09-08 19:55:20 -0600

    Use CRAN leiden (#68)
    And remove reticulate module loading. A warning is generated if the python modules are not availabile.

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-
-#' @noRd
-#' @importFrom reticulate import
-.onLoad <- function(libname, pkgname) {
-  # use superassignment to update global reference to scipy
-  leidenalg_py <<- reticulate::import("leidenalg", delay_load = TRUE)
-  igraph_py <<- reticulate::import("igraph", delay_load = TRUE)
-}
